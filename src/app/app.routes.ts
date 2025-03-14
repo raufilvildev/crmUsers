4398274d0b34817b74f56c358e2d8b96ad09ba96
Raúl Filigrana Villalba <raufilvil@gmail.com>
2025-03-14 22:01:41 +0100

    Creo todos los componentes y configuro el fichero de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
+import { UpdateUserComponent } from './pages/update-user/update-user.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: '/home'},
+    { path: 'home', component: HomeComponent },
+    { path: 'user/:id', component: UserViewComponent },
+    { path: 'newuser', component: NewUserComponent },
+    { path: 'updateuser/:id', component: UpdateUserComponent },
+    { path: '**', component: Error404Component}
+];
